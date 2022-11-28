8af9055d73105bfd055ba6c989f679a7ee81fd80
ddawg776 <ddawg776@gmail.com>
2022-11-28 11:13:26 -0600

    please god help us

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,6 +1,15 @@
-import React from "react";
-
+import React, {useState, useEffect} from "react";
+// View all products
 const Products = () => {
+    const [products, setProducts] = useState();
+    useEffect(() => {
+        async function getAllProducts(){
+            const getProducts = await fetch('https://gg-3pln.onrender.com/api/products')
+            const jsonProd = await getAllProducts.json();
+            console.log('this is json ', jsonProd)
+        }
+        getAllProducts();
+    }, [])
     return(
         <div>
             <h3>These are our products</h3>

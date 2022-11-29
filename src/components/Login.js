15904aafa42714e6811fa2ada56c1c6d4b3c22f0
William <wkeenze@yahoo.com>
2022-11-29 15:38:30 -0600

    Login/Register

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,70 @@
-import React from "react";
+import {useState} from "react"
+import {Link, useNavigate} from "react-router-dom"
 
 const Login = () => {
-    return(
-        <div>
-            <h3>Login Page</h3>
-        </div>
+   const [username, setUsername] = useState("")
+   const [password, setPassword] = useState("")
+    const navigate = useNavigate()
+
+   async function loginAPI (event) {
+    event.preventDefault()
+    try{
+        
+        const data = await fetch('https://gg-3pln.onrender.com/api/users/login', 
+        {
+            method : "POST",
+            headers : {
+                'Content-Type' : "application/json"
+            },body: JSON.stringify({
+                username,
+                password
+              })
+            
+        })
+        const results = await data.json()
+        // console.log(results)
+        if (results.token) {
+            localStorage.setItem("token", results.token)
+        } else {
+            alert (results.message)
+        }
+        if (results.token){
+            navigate("../")
+            alert(results.message)
+        }
+    } catch(error){
+        console.log(error)
+    }
+    
+   }
+   
+   function changePass (event) {
+    // console.log(password)
+    setPassword(event.target.value)
+    
+    }
+   function changeUser (event) {
+    // console.log(username)
+    setUsername(event.target.value)
+    
+    }
+    return (
+        <div className = "container">
+        <form onSubmit={loginAPI}>
+            <div className = "TypingInput">
+                <div>Username: </div>
+                <input type = "text" value = {username} onChange={changeUser}></input>
+            </div>
+            <div className = "TypingInput">
+                <div>Password: </div>
+                <input type = "password" value = {password} onChange={changePass}></input>
+            </div>
+            <div className = "submitcontain">
+               <button type = "submit">Login</button>
+            </div>
+        </form>
+        <div> <Link to = "/Register" className = "NavButton">Click here to Register</Link></div>
+      </div>
     )
 }
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,11 +1,86 @@
-import React from "react";
+import { useState } from "react"
+import {Link, useNavigate} from "react-router-dom"
 
 const Register = () => {
-    return(
-        <div>
-            <h3>Our registration page</h3>
-        </div>
-    )
+        const [username, setUsername] = useState("")
+        const [password, setPassword] = useState("")
+        const [email, setEmail] = useState("")
+        const [address, setAddress] = useState("")
+
+        const navigate = useNavigate()
+        
+        async function registerAPI (event) {
+            event.preventDefault()
+
+            try{
+                const data = await fetch('https://gg-3pln.onrender.com/api/users/register', 
+                {
+                    method : "POST",
+                    headers : {
+                        'Content-Type' : "application/json"
+                    }, body: JSON.stringify({
+                        username,
+                        password,
+                        email,
+                        address
+                      })
+                    
+                })
+                const results = await data.json()
+                    console.log(results)
+                if (results) {
+                    localStorage.setItem("token", results.token)   
+                } else {
+                    alert (results.message)
+                }
+                if (results){
+                     navigate("../")
+                }
+                
+            } catch(error){
+                console.log(error)
+            }
+            
+           }
+
+        function changePass (event) {
+         setPassword(event.target.value)
+        }
+        function changeUser (event) {
+         setUsername(event.target.value)
+        }
+        function changeEmail (event) {
+            setEmail(event.target.value)
+        } 
+        function changeAddress (event) {
+            setAddress(event.target.value)
+        }
+         return (
+            <div className = "container">
+            <form onSubmit={registerAPI}>
+                <div className = "TypingInput">
+                    <div>Enter Username: </div>
+                    <input type = "text" value = {username} onChange={changeUser}></input>
+                </div>
+                <div className = "TypingInput">
+                    <div>Enter Password: </div>
+                    <input type = "password" value = {password} onChange={changePass}></input>
+                </div>
+                <div className = "TypingInput">
+                    <div>Enter Email: </div>
+                    <input type = "email" value = {email} onChange={changeEmail}></input>
+                </div>
+                <div className = "TypingInput">
+                    <div>Enter Address: </div>
+                    <input type = "text" value = {address} onChange={changeAddress}></input>
+                </div>
+                <div className = "submitcontain">
+                   <button type = "submit">Register</button>
+                </div>
+            </form>
+          </div>
+     
+         ) 
 }
 
 export default Register

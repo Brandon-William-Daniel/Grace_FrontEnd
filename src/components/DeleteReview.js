71b9908fd2124542158db87b5f43bd6f4de23e53
Brandon Varney <b.varney03@gmail.com>
2022-12-01 15:51:13 -0500

    Update and delete review

--- src/components/DeleteReview.js
+++ src/components/DeleteReview.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { useParams } from "react-router-dom"
 
 const DeleteReview = () => {
+    const {id, userId} = useParams()
+    console.log("This is the product id:", id)
+    console.log("This is the user id:" , userId)
+    async function deleteReview() {
+        try {
+            const deleteReviewFetch = await fetch (`https://gg-3pln.onrender.com/api/reviews/deletereview/${id}/3`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${localStorage.getItem("token")}`
+                },
+            })
+            const deleteData = await deleteReviewFetch.json()
+            console.log("this is the delete data:", deleteData)
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
     return(
         <div>
-            <h3>To delete review</h3>
+            <button type="submit" onClick={deleteReview}>Delete this Review</button>
         </div>
     )
 }

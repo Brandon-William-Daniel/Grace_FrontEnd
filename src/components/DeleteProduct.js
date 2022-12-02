746454d04eab607a0aeef8f6a8ed7c4e46ffaf49
ddawg776 <ddawg776@gmail.com>
2022-12-02 11:10:41 -0600

    added better UI to deleteing a product

--- src/components/DeleteProduct.js
+++ src/components/DeleteProduct.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 
 const DeleteProduct = () => {
     const {productid} = useParams()
+    const [singleProd, setSingleProd] = useState()
+    const [flag, setFlag] = useState(false);
     const navigate = useNavigate()
     async function deleteProduct(event){
         event.preventDefault();
@@ -23,15 +25,34 @@ const DeleteProduct = () => {
         console.log(error)
     }
     }
-    return(
-        <div>
-            <form onSubmit={deleteProduct}>
-                <label>Are you Sure you want to delete this product?</label>
-                <br></br>
-                <input type='submit'></input>
-            </form>
-        </div>
-    )
+
+    useEffect(() => {
+        async function getSingleProd(){
+            const prodFetch = await fetch(`https://gg-3pln.onrender.com/api/products/${productid}`)
+            const singleJson = await prodFetch.json()
+            setSingleProd(singleJson.product)
+            if(singleJson){
+                setFlag(true)
+            }
+        }
+        getSingleProd()
+    }, [flag])
+    if(flag === true && singleProd.active){
+        return(
+            <div>
+                <form onSubmit={deleteProduct}>
+                    <label>Are you Sure you want to delete this product?</label>
+                    <p>{singleProd.title}</p>
+                    <br></br>
+                    <img src={singleProd.photo}></img>
+                    <br></br>
+                    <input type='submit'></input>
+                </form>
+            </div>
+        )
+    } else{
+        <p>Loading Data</p>
+    }
 }
 
 export default DeleteProduct

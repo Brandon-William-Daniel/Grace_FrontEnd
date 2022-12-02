9ded6d90d7c291d65aeb8ab6d6917256369d402a
ddawg776 <ddawg776@gmail.com>
2022-12-02 11:57:35 -0600

    changed add product page

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -10,8 +10,24 @@ const AddProduct = () => {
     const [cat, setCat] = useState();
     const [active, setActive] = useState(true);
     const navigate = useNavigate()
-    const {setProducts} = useOutletContext()
-    const{user} = useOutletContext()
+    const [user, setUser] = useState()
+    
+    useEffect(() => {
+        async function checkAdmin(){
+            const fetchData = await fetch(`https://gg-3pln.onrender.com/api/users/me`, {
+                method: 'GET',
+                headers: {
+                    "Content-Type": 'application/json',
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+            const jsonData = await fetchData.json()
+            if(jsonData){
+                setUser(jsonData)
+            }
+        }
+        checkAdmin()
+    }, [])
     async function newProduct(event){
         event.preventDefault();
         const prodFetch = await fetch(`https://gg-3pln.onrender.com/api/products/newproduct`, {
@@ -44,48 +60,56 @@ const AddProduct = () => {
         //     navigate('/')
         // }
     }
-    return(
+    if(user && user.isAdmin === true){
+        return(
+            <div>
+                <h3>Create a new product here!</h3>
+                <form onSubmit={newProduct}>
+                    <label>Title:</label>
+                    <input type='text' value={title} onChange={(event) => {
+                        console.log(event.target.value)
+                        setTitle(event.target.value)
+                    }}></input>
+                    <br></br>
+                    <label>Description:</label>
+                    <input type='text' value={description} onChange={(event) => {
+                        setDescription(event.target.value)
+                    }}></input>
+                    <br></br>
+                    <label>Price:</label>
+                    <input type='number' value={price} onChange={(event) => {
+                        setPrice(event.target.value)
+                    }}></input>
+                    <br></br>
+                    <label>invQty:</label>
+                    <input type='number' value={invQty} onChange={(event) => {
+                        setInvQty(event.target.value)
+                    }}></input>
+                    <br></br>
+                    <label>Photo URL:</label>
+                    <input type='text' value={photo} onChange={(event) => {
+                        setPhoto(event.target.value)
+                    }}></input>
+                    <br></br>
+                    <label>Catagory ID:</label>
+                    <input type='number' value={cat} onChange={(event) => {
+                        setCat(event.target.value)
+                    }}></input>
+                    <br></br>
+                    <label>Active?</label>
+                    <button onClick={() => {setActive(true)}}>True</button><button onClick={() => {setActive(false)}}>False</button>
+                    <br></br>
+                    <input type='submit'></input>
+                </form>
+            </div>
+        )
+    }else if(user.isAdmin === false){
+        return(
         <div>
-            <h3>Create a new product here!</h3>
-            <form onSubmit={newProduct}>
-                <label>Title:</label>
-                <input type='text' value={title} onChange={(event) => {
-                    console.log(event.target.value)
-                    setTitle(event.target.value)
-                }}></input>
-                <br></br>
-                <label>Description:</label>
-                <input type='text' value={description} onChange={(event) => {
-                    setDescription(event.target.value)
-                }}></input>
-                <br></br>
-                <label>Price:</label>
-                <input type='number' value={price} onChange={(event) => {
-                    setPrice(event.target.value)
-                }}></input>
-                <br></br>
-                <label>invQty:</label>
-                <input type='number' value={invQty} onChange={(event) => {
-                    setInvQty(event.target.value)
-                }}></input>
-                <br></br>
-                <label>Photo URL:</label>
-                <input type='text' value={photo} onChange={(event) => {
-                    setPhoto(event.target.value)
-                }}></input>
-                <br></br>
-                <label>Catagory ID:</label>
-                <input type='number' value={cat} onChange={(event) => {
-                    setCat(event.target.value)
-                }}></input>
-                <br></br>
-                <label>Active?</label>
-                <button onClick={() => {setActive(true)}}>True</button><button onClick={() => {setActive(false)}}>False</button>
-                <br></br>
-                <input type='submit'></input>
-            </form>
+            <h1>Admins Only Beyond this point</h1>
         </div>
-    )
+        )
+    }
 }
 
 export default AddProduct

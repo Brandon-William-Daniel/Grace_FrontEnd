6aa6013dd64be338deb196634be5b16b1c42ca68
ddawg776 <ddawg776@gmail.com>
2022-12-02 14:07:06 -0600

    testing remove from cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -21,19 +21,41 @@ const Cart = () => {
      }
      getCart()
     }, [])
+
+    async function removeFromCart(event){
+        event.preventDefault();
+        try {
+            const removeFetch = await fetch(`https://gg-3pln.onrender.com/api/orders/detail/${cart.id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+            const jsonFetc = await removeFetch.json()
+            console.log(jsonFetc)
+        } catch (error) {
+            console.log(error)
+        }
+    }
     console.log('this is cart', cart)
     return(
         <div>
             {cart ? cart.map((cart, idx) => {
                 if(cart.active){
             return(
-                <div>
-                    <div className="productsDiv"  key={idx}>
+                <div key={idx}>
+                    <div className="productsDiv">
                         <div>Title: {cart.title}</div>
                         <div>Description: {cart.description}</div>
                         <div>Quanitity: {cart.quantity} </div>
                         <div>Price: ${cart.price}</div>
                         <br></br>
+                        <form onSubmit={removeFromCart}>
+                            <button type='submit' onClick={() => {
+                                console.log('cart', cart)
+                            }}>Remove from cart</button>
+                        </form>
                     </div>
                 </div>
             )}
